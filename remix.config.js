f858fb2dcb47641ccf072c9b60e4bb7ae78fc74b
TadjerouniMohamedAdel <mohnagato@gmail.com>
2024-02-04 10:28:47 +0100

    update remix config

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,8 @@
+import { config } from '@netlify/remix-adapter';
+
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  ...(process.env.NODE_ENV === 'production' ? config : undefined),
   ignoredRouteFiles: ['**/.*'],
   serverDependenciesToBundle: [/^swiper/],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 };
