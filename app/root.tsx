ca95cb0f89e4a9cfe279ee9bb533b375bf33d2fe
TadjerouniMohamedAdel <mohnagato@gmail.com>
2024-02-11 13:34:34 +0100

    update root to include boxicon in links

--- app/root.tsx
+++ app/root.tsx
@@ -10,14 +10,11 @@ import {
 } from '@remix-run/react';
 import rootcss from './root.css';
 
-const boxicons = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
-
 export const links: LinksFunction = () => [
   ...(cssBundleHref
     ? [
         { rel: 'stylesheet', href: cssBundleHref },
         { rel: 'stylesheet', href: rootcss },
-        { rel: 'stylesheet', href: boxicons },
       ]
     : []),
 ];
@@ -28,6 +25,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
