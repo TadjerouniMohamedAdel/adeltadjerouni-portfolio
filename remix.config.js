7490076d27a949d3180af7e24bbdef9a5a034d30
TadjerouniMohamedAdel <mohnagato@gmail.com>
2024-02-11 16:05:56 +0100

    test fix encoding

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,5 @@ import { config } from '@netlify/remix-adapter';
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...(process.env.NODE_ENV === 'production' ? config : undefined),
-  ignoredRouteFiles: ['**/.*'],
   serverDependenciesToBundle: [/^swiper/],
 };
