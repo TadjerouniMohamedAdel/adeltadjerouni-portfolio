af20facd3875856b7afd2dc98f20f141dfa5e5a1
TadjerouniMohamedAdel <mohnagato@gmail.com>
2024-02-11 14:12:08 +0100

    Revert "install globals in server.ts"
    This reverts commit 7ebf50b964ae4b4ff6d62dc7d420355174fa08d8.

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import * as build from "@remix-run/dev/server-build";
 import { createRequestHandler } from "@netlify/remix-adapter";
-import { installGlobals } from '@remix-run/node';
-installGlobals()
+
 const handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
