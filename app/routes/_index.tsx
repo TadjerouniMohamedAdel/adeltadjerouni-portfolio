52b150e72daf4106619129fd363a5965305180c0
TadjerouniMohamedAdel <mohnagato@gmail.com>
2024-02-11 16:02:48 +0100

    Revert "add headers to index page"
    This reverts commit cfbbea1d4375f4909d8f1c7dd62d1dae8c840678.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,22 +15,6 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: indexcss },
   { rel: 'stylesheet', href: swipercss },
 ];
-export function headers({
-  loaderHeaders,
-  parentHeaders,
-}: {
-  loaderHeaders: Headers;
-  parentHeaders: Headers;
-}) {
-  console.log(
-    'This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header'
-  );
-  return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
-    'Cache-Control': 'public, max-age=60, s-maxage=60',
-  };
-}
 export const meta: MetaFunction = () => {
   const title = 'Adel Mohamed Tadjerouni Portfolio';
   const description =
